163e2303fde2e21878a088118e86577478920473
ntkb4 <91574282+ntkb4@users.noreply.github.com>
2021-10-03 12:24:36 -0400

    Update Team3Demo.java
    :)

--- src/team3/Team3Demo.java
+++ src/team3/Team3Demo.java
@@ -5,6 +5,7 @@ public class Team3Demo {
 	public static void main(String[] args) {
 		System.out.println("TEam3Demo");
 		System.out.println("trap...");
+		System.out.println("More Trap....");
 	}
 
 }
