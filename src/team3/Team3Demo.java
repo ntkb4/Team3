bf97c347d02ec1e977fcef41e317e638bb2d5e76
shahl <shahl@192.168.1.239>
2021-10-03 12:13:52 -0400

    lets see the changes

--- src/team3/Team3Demo.java
+++ src/team3/Team3Demo.java
@@ -4,6 +4,9 @@ public class Team3Demo {
 
 	public static void main(String[] args) {
 		System.out.println("TEam3Demo");
+		
+		// lets see the changes
+		System.out.println("Hello World");
 	}
 
 }
