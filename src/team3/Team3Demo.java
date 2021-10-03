64ad18132952b3ed3dc26413901db43063b22592
shahl <shahl@192.168.1.239>
2021-10-03 12:24:35 -0400

    hiii

--- src/team3/Team3Demo.java
+++ src/team3/Team3Demo.java
@@ -5,11 +5,12 @@ public class Team3Demo {
 	public static void main(String[] args) {
 		System.out.println("TEam3Demo");
 		
-		System.out.println("Hello Team Three");
-		
 		// lets see the changes
 		System.out.println("Hello World");
 
+		
+		// seccond changses 
+		System.out.println("Hello Team 3");
 	}
 
 }
